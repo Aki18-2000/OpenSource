4a146ce1954a58dc4cddcd6712f1ba17fdeb2de4
Aki18-2000 <makshaya206@gmail.com>
2025-03-19 10:53:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,9 +6,56 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_security_group" "allow_ssh_http" {
@@ -39,7 +86,7 @@ resource "aws_security_group" "allow_ssh_http" {
 resource "aws_instance" "openproject" {
   ami           = "ami-0c55b159cbfafe1f0"  # Change to your preferred AMI
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.main.id
+  subnet_id     = aws_subnet.private.id
   security_groups = [aws_security_group.allow_ssh_http.name]
 
   user_data = <<-EOF
@@ -61,7 +108,7 @@ resource "aws_lb" "app_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.allow_ssh_http.id]
-  subnets            = [aws_subnet.main.id]
+  subnets            = [aws_subnet.public.id]
 
   enable_deletion_protection = false
 }
